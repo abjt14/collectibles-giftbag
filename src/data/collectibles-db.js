62045a2ba43db0d0278e062dcbf79afdadafa5b0
Abhijeet Singh <abhijeetsinghmain@gmail.com>
2024-07-23 19:59:17 -0700

    mock database with collectibles information

---
+++ src/data/collectibles-db.js
@@ -0,0 +1,34 @@
+export const DATABASE = [
+  {
+    slug: "0x1",
+    width: 270,
+    height: 463,
+    alt: "shallow greens and the deep blue sea",
+    selected: false,
+    owned: true,
+  },
+  {
+    slug: "0x2",
+    width: 270,
+    height: 180,
+    alt: "calming aurora",
+    selected: false,
+    owned: true,
+  },
+  {
+    slug: "0x3",
+    width: 270,
+    height: 180,
+    alt: "nebulas galore",
+    selected: false,
+    owned: true,
+  },
+  {
+    slug: "0x4",
+    width: 270,
+    height: 405,
+    alt: "the sun versus a sunny side up",
+    selected: false,
+    owned: true,
+  },
+];
