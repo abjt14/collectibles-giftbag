33f18f1a311ae6886fd2a781786e1c08d6fff685
Abhijeet Singh <abhijeetsinghmain@gmail.com>
2024-07-23 20:04:34 -0700

    main structure, state management, and framer motion layout setup

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,85 @@
-import Image from "next/image";
+"use client";
+
+import React from "react";
+import { DATABASE } from "@/data/collectibles-db";
+import { produce } from "immer";
+import Collectibles from "@/components/Collectibles";
+import DynamicButton from "@/components/DynamicButton";
+import BlurOverlay from "@/components/BlurOverlay";
+import { motion, AnimatePresence } from "framer-motion";
+import GiftBag from "@/components/GiftBag";
 
 export default function Home() {
+  const [collectibleArray, setCollectibleArray] = React.useState(DATABASE);
+  const [progress, setProgress] = React.useState("initial"); // initial | confirm | authenticate | processing | completed
+
+  React.useEffect(() => {
+    let timeoutID;
+
+    // for demo purposes only
+    // emulating a real world api response delay using a settimeout
+    if (progress === "processing") {
+      timeoutID = window.setTimeout(() => {
+        setProgress("completed");
+      }, 2000);
+    }
+
+    if (progress === "completed") {
+      // once the progress has reached "completed" status
+      // we find the collectibles that were selected but are still owned
+      // and make their owned status false to show the remaining collectibles on home
+      setCollectibleArray((prev) => {
+        return produce(prev, (draftState) => {
+          draftState.forEach((state) => {
+            if (state.selected && state.owned) {
+              state.owned = false;
+            }
+          });
+        });
+      });
+
+      // for demo purposes only
+      // checking if we have any remaining owned cards after the progress reaches "completed" status
+      // we reset the data and the progress to loop the interaction
+      const ownedCardCount = collectibleArray.filter(
+        (card) => card.owned
+      ).length;
+      timeoutID = window.setTimeout(() => {
+        setProgress("initial");
+        if (ownedCardCount === 0) {
+          setCollectibleArray(DATABASE);
+        }
+      }, 2000);
+    }
+  }, [progress, collectibleArray]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <main className="min-h-svh flex flex-col justify-center items-center gap-16">
+      <motion.div
+        layout
+        layoutRoot
+        className="size-80 relative grid grid-cols-2 mx-auto justify-items-center"
+      >
+        {/* animate presence enables us to have an exit animation when unmounting components similar to native apps */}
+        <AnimatePresence>
+          {progress !== "initial" && <BlurOverlay />}
+        </AnimatePresence>
+        <Collectibles
+          collectibleArray={collectibleArray}
+          setCollectibleArray={setCollectibleArray}
+          progress={progress}
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <AnimatePresence>
+          {progress !== "initial" && (
+            <GiftBag triggerCompletedAnimation={progress === "completed"} />
+          )}
+        </AnimatePresence>
+      </motion.div>
+      <DynamicButton
+        progress={progress}
+        setProgress={setProgress}
+        collectibleArray={collectibleArray}
+      />
     </main>
   );
 }
