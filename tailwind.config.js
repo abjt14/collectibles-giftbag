5e5865ef610ced222db3a0062373d256615baaa8
Abhijeet Singh <abhijeetsinghmain@gmail.com>
2024-07-23 20:00:57 -0700

    added custom reverse spin animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,15 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      keyframes: {
+        "spin-reverse": {
+          from: { transform: "rotate(0deg)" },
+          to: { transform: "rotate(-360deg)" },
+        },
+      },
+      animation: {
+        "spin-reverse": "spin-reverse 1s linear infinite",
+      },
     },
   },
   plugins: [],
